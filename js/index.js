00387a6a451cab51dc8b7ecd09651cc2d6d85576
Akhlas789 <akhlasuddin54321@gmail.com>
2023-10-14 14:47:10 +0600

    category added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 const handaleCategory = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/news/categories')
     const data = await res.json()
-    console.log(data.data)
+
+    const tabContainer = document.getElementById('tab-container')
+    data.data.news_category.slice(0, 3).forEach(category => {
+        const div = document.createElement('div')
+        div.innerHTML = `
+            <a onclick ="handaleLoadNews('${category.category_id}')" class="tab">${category.category_name}</a>
+        `
+        tabContainer.appendChild(div)
+    });
 }
 handaleCategory()
 
+const handaleLoadNews = (categoryId) => {
+    console.log(categoryId)
+}
+handaleLoadNews()
 
